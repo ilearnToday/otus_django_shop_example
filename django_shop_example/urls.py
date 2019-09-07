224c544e92a35b3ecf39579c127731ba32c4cdee
ilearnToday <goodeitime@gmail.com>
2019-09-07 14:52:42 +0200

    End main page page-proofs

--- django_shop_example/urls.py
+++ django_shop_example/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop_example.urls')),
+    path('shop/', include('shop_example.urls')),
 
 ]
--- shop_example/urls.py
+++ shop_example/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'shop_example'
 
 urlpatterns = [
-    path('shop/', views.MainPageView.as_view()),
-    # path('shop/computers/<int:computer>', views.ComputerPageView.as_view())
+    path('', views.MainPageView.as_view()),
+    # path('computers/<int:computer>', views.ComputerPageView.as_view())
 
 ]
